06c4d854102c3fde5296b3d8c4b02bd78d6aeef1
maikelmclauflin <michael.j.mclaughlin20@gmail.com>
2020-06-09 19:34:39 -0500

    fixed ttl

--- build.js
+++ build.js
@@ -6,10 +6,10 @@ module.exports = main
 async function main() {
   const lastBuild = await files.read('last-build')
   const now = new Date()
-  if (lastBuild && new Date(+lastBuild) > (now - HOUR)) {
+  if (lastBuild) {
     return
   }
   await fillCalendar(process.env.ADDRESS)
   const choppedTime = now - (now % HOUR)
-  await files.write('last-build', choppedTime, ['EX', 600])
+  await files.write('last-build', choppedTime, ['EX', 3600])
 }
